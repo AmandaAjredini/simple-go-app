7fffc0c280ff0bc2fa5ec8fbdd829ad2bbfa5d0a
AmandaAjredini <amanda_ajredini@hotmail.com>
2025-08-06 12:47:43 +0100

    Initial commit for simple Go app

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(x http.ResponseWriter, r *http.Request) {
+	log.Println("Received a request at /")
+	fmt.Fprintf(w, "Hello from OpenShift!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Println("Starting server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
